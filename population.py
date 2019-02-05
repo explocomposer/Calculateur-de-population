e02ebc19be34919dc6b2e6b6f9e7b267131fb713
explocomposer <47303915+explocomposer@users.noreply.github.com>
2019-02-05 10:51:54 +0100

    Update population.py
    Ajout d'un système automatisant le calcul de génération suivante

--- population.py
+++ population.py
@@ -1,7 +1,3 @@
-# (STARTING POPULATION + (BIRTHRATE x WOMEN) ) - PAST GENERATION
-
-population_start = int(input("Entrez une valeur correspondant à la population initiale: "))
-birthrate = int(input("Entrez une valeur correspondant à la moyenne du nombre d'enfant par couple: "))
 women = int(input("Entrez une valeur correspondant au nombre de femmes moyen dans la population: "))
 past_generation = int(input("Entrez une valeur correspondant à la population précédente: "))
 
@@ -10,4 +6,16 @@ print(population_start,birthrate,women,past_generation)
 result = birthrate * women + population_start - past_generation
 
 print("la population actuelle est de",result,"habitants.")
+
+while True:
+    past_generation = int(population_start)
+    population_start = int(result)
+    women = int(population_start / 2)
+    birthrate = int(input("Entrez une valeur correspondant au nombre d'enfant moyen par couple: "))
+
+    print(population_start,birthrate,women,past_generation)
+
+    result = birthrate * women + population_start - past_generation
+
+    print("la population actuelle est de",result,"habitants.")
 
