9dd752976bb57861b2b74b220fb45ac522bb85e2
explocomposer <47303915+explocomposer@users.noreply.github.com>
2019-02-05 11:08:35 +0100

    Update population.py

--- population.py
+++ population.py
@@ -1,28 +1,28 @@
 
 population_start = int(input("Entrez une valeur correspondant à la population initiale: "))
-birthrate = int(input("Entrez une valeur correspondant à la moyenne du nombre d'enfant par couple: "))
 women = int(input("Entrez une valeur correspondant au nombre de femmes moyen dans la population: "))
+birthrate = int(input("Entrez une valeur correspondant à la moyenne du nombre d'enfant par couple: "))
 past_generation = int(input("Entrez une valeur correspondant à la population précédente: "))
 
-print(population_start,birthrate,women,past_generation)
+print(population_start,women,birthrate,past_generation)
 
 result = birthrate * women + population_start - past_generation
 
-print("la population actuelle est de",result,"habitants.")
+print("""la population actuelle est de",result,"habitants.
+Calcul de la génération suivante:""")
 
 while True:
     past_generation = int(population_start)
     population_start = int(result)
-    input("Souhaitez-vous influer sur le nombre de femmes pour la génération suivante? (oui,non)")
-    if "oui":
+    change = input("Souhaitez-vous influer sur le nombre de femmes pour la génération suivante? (oui,non)")
+    if change == "oui":
         women = int(input("Entrez une nouvelle valeur: "))
-    elif "non":
+    elif change == "non":
         women = int(population_start / 2)
     birthrate = int(input("Entrez une valeur correspondant au nombre d'enfant moyen par couple: "))
 
-    print(population_start,birthrate,women,past_generation)
+    print(population_start,women,birthrate,past_generation)
 
     result = birthrate * women + population_start - past_generation
 
     print("la population actuelle est de",result,"habitants.")
-
