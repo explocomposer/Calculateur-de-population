f902cad0ed67ae34dca1e44fd0146b8facca929a
explocomposer <47303915+explocomposer@users.noreply.github.com>
2019-02-05 10:57:41 +0100

    Update population.py
    Amélioration de l'algorythme

--- population.py
+++ population.py
@@ -1,3 +1,6 @@
+
+population_start = int(input("Entrez une valeur correspondant à la population initiale: "))
+birthrate = int(input("Entrez une valeur correspondant à la moyenne du nombre d'enfant par couple: "))
 women = int(input("Entrez une valeur correspondant au nombre de femmes moyen dans la population: "))
 past_generation = int(input("Entrez une valeur correspondant à la population précédente: "))
 
@@ -10,7 +13,11 @@ print("la population actuelle est de",result,"habitants.")
 while True:
     past_generation = int(population_start)
     population_start = int(result)
-    women = int(population_start / 2)
+    input("Souhaitez-vous influer sur le nombre de femmes pour la génération suivante? (oui,non)")
+    if "oui":
+        women = int(input("Entrez une nouvelle valeur: "))
+    elif "non":
+        women = int(population_start / 2)
     birthrate = int(input("Entrez une valeur correspondant au nombre d'enfant moyen par couple: "))
 
     print(population_start,birthrate,women,past_generation)
